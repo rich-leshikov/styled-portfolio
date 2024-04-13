10b3759631df81c745841a136d1c3fdf58dc61c3
Richard Leshik <richardleshik@gmail.com>
2024-04-13 23:01:28 +0300

    refactor: add index exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
-import {GlobalStyles} from './styles/GlobalStyles'
+import {GlobalStyles} from './styles'
 
 ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
