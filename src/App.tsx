45d10a10bf5d310be0ff4ff3b60c5f99b048848f
Richard Leshik <richardleshik@gmail.com>
2024-04-13 21:11:00 +0300

    refactor: update index and App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import './App.css';
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 
-function App() {
-    return (
-        <div className="App">
-            <Title>Welcome to IT-INCUBATOR</Title>
-        </div>
-    );
+export function App() {
+	return (
+		<div className="App">
+			<Title>Welcome to IT-INCUBATOR</Title>
+		</div>
+	)
 }
 
-export default App;
-
 const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
+  font-size: 1.5em;
+  text-align: center;
+  color: #e91e63;
+`
